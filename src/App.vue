7e0f78e90ae8d9117f48a2b3eb1229688eac38c1
Julian Valle <julian.valle@enovational.com>
2022-05-18 21:15:10 -0400

    links added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import MainCard from './components/MainCard.vue';
+import selfieImage from './assets/images/image01.jpg';
+import lenaLinks from './lena-links.json'
+
+const links = lenaLinks
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <main>
+    <MainCard :selfieImage="selfieImage" handle="@lenazerlav" :links="links" />
+  </main>
 </template>
 
-<style>
+<style lang="scss">
+main {
+  // background-color: var(--primary);
+  min-height: 100vh;
+  padding: 2rem;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card">
+    <img :src="props.selfieImage" alt="selfie" class="selfie-image" />
+    <h1>{{ handle }}</h1>
+    <ul class="link-list">
+      <li v-for="link in links">
+        <a :href="link.href" class="links">{{ link.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  selfieImage: String,
+  handle: String,
+  links: Object,
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 3px solid white;
+  border-radius: 1rem;
+  max-width: 24rem;
+  min-width: 18rem;
+  width: 100%;
+  margin: 0 auto;
+  color: white;
+  text-align: center;
+  gap: 1rem;
+}
+.selfie-image {
+  width: 100%;
+  border-radius: 1rem;
+}
+.link-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.5rem;
+}
+.links {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3rem;
+  border-radius: 0.5rem;
+  background-color: #363034;
+  color: white;
+  padding: 0 1.5rem;
+  text-transform: uppercase;
+  transition: ease-in all .2s;
+  &:hover {
+    background-color: lightskyblue;
+    color: black;
+  }
+}
+</style>
